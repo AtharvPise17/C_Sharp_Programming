746c801f4d6c5d016b77fc15f80987afcc34b11c
Atharv <atharvpise16112001@gmail.com>
2022-10-25 02:37:51 -1200

    Assignment-1

---
+++ Assignments/Assignment 1/YC_Student_Management_System_1/YC_Student_Management_System/frm_View_Student_List.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace YC_Student_Management_System
+{
+    public partial class frm_View_Student_List : Form
+    {
+        public frm_View_Student_List()
+        {
+            InitializeComponent();
+        }
+        private void btn_Log_Out_Click(object sender, EventArgs e)
+        {
+            frm_Login obj = new frm_Login();
+            obj.Show();
+            this.Hide();
+        }
+        private void btn_Add_New_Student_Click(object sender, EventArgs e)
+        {
+            frm_Add_New_Student_Details obj = new frm_Add_New_Student_Details();
+            obj.Show();
+            this.Hide();
+        }
+    }
+}
